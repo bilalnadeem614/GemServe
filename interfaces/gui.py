fa786c8474afa67b8d27264f33ff32fea1a1dde5
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-08 23:25:52 +0500

    Made a change nothing much.

--- interfaces/gui.py
+++ interfaces/gui.py
@@ -1,7 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
